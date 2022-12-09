0ae391a9d8e6a8e5b4eec5da0808e6b5e1d443fc
Carlin Starrs <carlinstarrs@gmaiil.com>
2022-12-09 08:48:20 -0800

    day7 cleanup

--- day07/day07.R
+++ day07/day07.R
@@ -1,4 +1,3 @@
-
 library("tidyverse")
 example <- c("$ cd /",
              "$ ls",
@@ -25,46 +24,48 @@ example <- c("$ cd /",
              "7214296 k")
 
 input <- readLines("day07/input.txt")
-dat <- example
 
+unwravel_filetree <- function(dat){
+  instructions <- setNames(which(grepl("^\\$", dat)), dat[grepl("^\\$", dat)])
+  
+  filepaths <- list()
+  
+  imap(instructions, function(id, instruction){
+    if(grepl("cd \\/", instruction)){
+      curdir <<- "/"
+    } else if(grepl("cd \\.\\.", instruction)){
+      #move out one level
+      curdir <<- str_remove(str_remove(curdir, "([^\\/]+$)"), "\\/$")
+    } else if(grepl("cd .+", instruction)){
+      #move to directory
+      curdir <<- file.path(curdir, str_extract(instruction, "(?<=\\$ cd ).+"))
+    } else if(grepl("ls", instruction)){
+      #get filenames and sizes below until next instruction
+      next_id <- replace_na(instructions[instructions > id][1], length(dat)+1)
+      result <- dat[(id+1):(next_id-1)]
+      files <- result[!grepl("dir", result)]
+      dirs <- result[grepl("dir", result)]
+      filepaths[[curdir]] <<- c(files)
+    }
+  })
+  
+  bbs <- list()
+  
+  imap(filepaths, function(files, dir){
+    out <- filepaths[grepl(str_replace_all(paste0("^", str_replace(dir, "/$", "")), "\\/", "\\\\/"), names(filepaths))] %>% 
+      unlist() %>% 
+      str_split(" ") %>% 
+      map(~as.numeric(.x[1])) %>% 
+      reduce(sum)
+    bbs <<- bind_rows(bbs, tibble("dir" = dir, 
+                                  "size" = out))
+  })
+  
+  return(bbs)
+}
 
-dat <- input
-
-instructions <- setNames(which(grepl("^\\$", dat)), dat[grepl("^\\$", dat)])
-
-filepaths <- list()
-
-imap(instructions, function(id, instruction){
-  if(grepl("cd \\/", instruction)){
-    curdir <<- "/"
-  } else if(grepl("cd \\.\\.", instruction)){
-    #move out one level
-    curdir <<- str_remove(str_remove(curdir, "([^\\/]+$)"), "\\/$")
-  } else if(grepl("cd .+", instruction)){
-    #move to directory
-    #curdir <<- paste0(str_remove(curdir, "\\/$"), "/dir ", str_extract(instruction, "(?<=\\$ cd dir).+"))
-    curdir <<- file.path(curdir, str_extract(instruction, "(?<=\\$ cd ).+"))
-  } else if(grepl("ls", instruction)){
-    #get filenames and sizes below until next instruction
-    next_id <- replace_na(instructions[instructions > id][1], length(dat)+1)
-    result <- dat[(id+1):(next_id-1)]
-    files <- result[!grepl("dir", result)]
-    dirs <- result[grepl("dir", result)]
-    filepaths[[curdir]] <<- c(files)
-  }
-})
-
-bbs <- list()
-
-imap(filepaths, function(files, dir){
-  out <- filepaths[grepl(str_replace_all(paste0("^", str_replace(dir, "/$", "")), "\\/", "\\\\/"), names(filepaths))] %>% 
-    unlist() %>% 
-    str_split(" ") %>% 
-    map(~as.numeric(.x[1])) %>% 
-    reduce(sum)
-  bbs <<- bind_rows(bbs, tibble("dir" = dir, 
-                                "size" = out))
-})
+bbs <- unwravel_filetree(example)
+bbs <- unwravel_filetree(input)
 
 sum(bbs$size[bbs$size < 100000])
 
