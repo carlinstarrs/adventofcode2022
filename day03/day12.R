3b1f0528bb952c379daffebd94047971ee31004e
Carlin Starrs <carlinstarrs@gmaiil.com>
2022-12-14 14:46:42 -0800

    day 12 done!

--- day03/day12.R
+++ day03/day12.R
@@ -7,44 +7,77 @@ example <- "day12/example.txt"
 dat <- example
 dat <- input
 
-
-mm <- readLines(dat) %>% 
-  map_dfr(~setNames(strsplit(.x, "")[[1]], paste0("X", 1:nchar(.x)))) %>% 
-  as.matrix() 
-
 dict <- setNames(c(1, 1:length(letters), 26), c("S", letters, "E"))
 
-ii <- matrix(1:length(mm), ncol = ncol(mm))
+tick <- Sys.time()
+gridvals <- readLines(dat) %>% 
+  map_dfr(~setNames(strsplit(.x, "")[[1]], paste0("X", 1:nchar(.x)))) %>% 
+  rowid_to_column("x") %>% 
+  pivot_longer(-x, 
+               names_to = "y",
+               names_pattern = "(\\d+$)") %>% 
+  mutate(y = as.numeric(y), 
+         elev = dict[value]) %>% 
+  rowid_to_column("id") 
 
-weights <- imap_dfr(mm, function(val, id){
-  # if(id == 3355) browser()
-  x <- col(mm)[id]
-  y <- row(mm)[id]
+adjvals <- gridvals %>% 
+  group_by(x) %>% 
+  mutate(lx = x, 
+         ly = ifelse(y-1 >= min(y), y-1, NA), 
+         rx = x, 
+         ry = ifelse(y+1 <= max(y), y+1, NA)) %>% 
+  group_by(y) %>% 
+  mutate(ux = ifelse(x-1 >= min(x), x-1, NA), 
+         uy = y, 
+         dx = ifelse(x+1 <= max(x), x+1, NA), 
+         dy = y) %>%
+  ungroup() %>% 
+  select(id, matches(".x$|.y$")) %>% 
+  pivot_longer(-id) %>% 
+  mutate(dir = str_extract(name, "^."), 
+         coord = str_extract(name, ".$")) %>% 
+  group_by(id, dir) %>% 
+  filter(!any(is.na(value))) %>% 
+  select(-name) %>% 
+  pivot_wider(names_from = coord, 
+              values_from = value) %>% 
+  left_join(gridvals %>% select(x, y, value, elev, to_id = id), by = c("x", "y")) %>% 
+  rename(from_id = id) %>% 
+  rename_with(~paste0("to_", .x), .cols = -c(from_id, to_id))
 
-  adjs <- list(c(y, x+1),  
-               c(y, x-1), 
-               c(y+1, x), 
-               c(y-1, x))
-  
-  adjs <- discard(adjs, ~.x[1] > max(row(mm)) | .x[1] < min(row(mm)))
-  adjs <- discard(adjs, ~.x[2] > max(col(mm)) | .x[2] < min(col(mm)))
-  
-  paths <- tibble(from = id, 
-                  to =  ii[matrix(unlist(adjs), ncol = 2, byrow = TRUE)], 
-                  from_val = val, 
-                  to_val = mm[matrix(unlist(adjs), ncol = 2, byrow = TRUE)]) %>% 
-    mutate(elev = dict[to_val] - dict[from_val])
-  paths[paths$elev <= 1,]
+valid_moves <- gridvals %>% 
+  rename_with(~paste0("from_", .x)) %>% 
+  left_join(adjvals, by = c("from_id")) %>% 
+  mutate(elev_diff = to_elev - from_elev) %>% 
+  filter(elev_diff <= 1)
+
+
+#part 1
+ggraph <- graph_from_edgelist(valid_moves %>% 
+                      select(from_id, to_id) %>% 
+                      as.matrix()) 
+
+g2 <- ggraph %>% 
+  shortest_paths(from = gridvals$id[gridvals$value == "S"], to = gridvals$id[gridvals$value == "E"]) %>% 
+  .$vpath 
+
+map_chr(unlist(g2), ~gridvals$value[gridvals$id == .x])
+
+length(g2[[1]]) - 1
+
+#part2 
+out <- map_dbl(gridvals$id[gridvals$value == "a"], function(start){
+  ggraph %>% 
+    shortest_paths(from = start, to = gridvals$id[gridvals$value == "E"]) %>% 
+    .$vpath %>%
+    unlist() %>% 
+    length()
 }) 
 
-aa <- graph_from_data_frame(weights %>% select(from, to))
+min(out[out > 0]) - 1
 
 
-g2 <- shortest_paths(graph_from_data_frame(weights %>% select(from, to)), from = which(mm == "S"), to = which(mm == "E"))
+tock <- Sys.time()
 
-mm[g2$vpath[[1]]]
-
-length(mm[g2$vpath[[1]]])-1
-
-test <- anti_join(weights, edgelist, by= c("from" = "Var1", "to" = "Var2"))
+tock - tick
 
